df5342923df389a93c1c0e5d8aa0415ccf6e7626
Matt Burke <spraints@gmail.com>
2021-10-02 20:39:47 -0400

    Rework exe
    with an arg, dump it
    with no args, run (tbd) interactive prompt
    anything else, show usage

---
+++ interactive.go
@@ -0,0 +1,5 @@
+package main
+
+func runInteractive() error {
+	return nil
+}
--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"log"
 	"os"
 
@@ -11,35 +12,56 @@ import (
 )
 
 func main() {
-	f, err := os.Open(os.Args[len(os.Args)-1])
+	switch len(os.Args) {
+	case 1:
+		finish(runInteractive())
+	case 2:
+		finish(dump(os.Args[1]))
+	default:
+		fmt.Printf("Usage: %s [FILE]\n", os.Args[0])
+		os.Exit(1)
+	}
+}
+
+func finish(err error) {
 	if err != nil {
-		log.Fatal(err)
+		fmt.Printf("%v\n", err)
+		os.Exit(1)
+	}
+}
+
+func dump(path string) error {
+	f, err := os.Open(path)
+	if err != nil {
+		return err
 	}
 	l, err := lmsp.ReadFile(f)
 	if err != nil {
-		log.Fatal(err)
+		return err
 	}
 	man, err := l.Manifest()
 	if err != nil {
-		log.Fatal(err)
+		return err
 	}
 	spew.Dump(man)
 
 	proj, err := l.Project()
 	if err != nil {
-		log.Fatal(err)
+		return err
 	}
 	spew.Dump(proj)
 
 	log.Print("writing JSON back out to 'testing.json'...")
 	f, err = os.Create("testing.json")
 	if err != nil {
-		log.Fatal(err)
+		return err
 	}
 	defer f.Close()
 	if err := json.NewEncoder(f).Encode(proj); err != nil {
-		log.Fatal(err)
+		return err
 	}
 
 	// todo later - print out programs in pybricks
+
+	return nil
 }
