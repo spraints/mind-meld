e82ca5e6e0e99930678a41694599f0cb046cae8c
Matt Burke <spraints@gmail.com>
2021-11-01 22:10:26 -0400

    Start redoing it

--- lmsdump/lmsdump.go
+++ lmsdump/lmsdump.go
@@ -10,7 +10,102 @@ import (
 func Dump(w io.Writer, proj lmsp.Project) {
 	for _, target := range proj.Targets {
 		fmt.Fprintf(w, "target: %s\n", target.Name)
-		dumpTarget(w, target)
+		visitTarget(w, target)
+	}
+}
+
+func visitTarget(w io.Writer, target lmsp.ProjectTarget) {
+	for _, id := range target.GetRootBlockIDs() {
+		fmt.Fprintln(w, "-------------------")
+		visitBlock(w, target, id)
+	}
+	// todo - other fields of ProjectTarget.
+}
+
+func visitBlock(w io.Writer, target lmsp.ProjectTarget, id lmsp.ProjectBlockID) {
+	block := target.Blocks[id].(*lmsp.ProjectBlockObject)
+	switch block.Opcode {
+	case "flippermoremotor_motorSetDegreeCounted":
+		visitMoreMotorSetDegreeCounted(w, target, block)
+	case "flippermoremotor_multiple-port-selector":
+		visitMoreMotorMultiplePortSelector(w, target, block)
+	case "flippersensors_resetYaw":
+		fmt.Fprintln(w, "resetYaw()")
+	case "procedures_call":
+		visitProcedureCall(w, target, block)
+	case "procedures_definition":
+		visitProcedureDefinition(w, target, block)
+	case "procedures_prototype":
+		visitProcedurePrototype(w, target, block)
+	default:
+		fmt.Fprintf(w, "!!!TODO %v!!!\n", block.Opcode)
+		return
+	}
+	if block.Next != nil {
+		visitBlock(w, target, *block.Next)
+	}
+}
+
+func visitProcedureCall(w io.Writer, target lmsp.ProjectTarget, block *lmsp.ProjectBlockObject) {
+	fmt.Fprintf(w, "%s %s(", block.Mutation.ProcCode, block.Mutation.ArgumentIDs)
+	inputs := block.Inputs.(map[string]interface{})
+	first := true
+	for id := range inputs {
+		if !first {
+			fmt.Fprint(w, ", ")
+		}
+		first = false
+		fmt.Fprintf(w, "%s: ", id)
+		visitInput(w, target, block, id)
+	}
+	fmt.Fprintf(w, ")\n")
+}
+
+func visitProcedureDefinition(w io.Writer, target lmsp.ProjectTarget, block *lmsp.ProjectBlockObject) {
+	fmt.Fprint(w, "def ")
+	visitInput(w, target, block, "custom_block")
+}
+
+func visitProcedurePrototype(w io.Writer, target lmsp.ProjectTarget, block *lmsp.ProjectBlockObject) {
+	fmt.Fprintf(w, "%s %s\n", block.Mutation.ProcCode, block.Mutation.ArgumentNames)
+	// Inputs is redundant with argument names.
+}
+
+func visitMoreMotorSetDegreeCounted(w io.Writer, target lmsp.ProjectTarget, block *lmsp.ProjectBlockObject) {
+	fmt.Fprint(w, "setDegreesCounted(port: ")
+	visitInput(w, target, block, "PORT")
+	fmt.Fprint(w, ", value: ")
+	visitInput(w, target, block, "VALUE")
+	fmt.Fprintln(w, ")")
+}
+
+func visitMoreMotorMultiplePortSelector(w io.Writer, target lmsp.ProjectTarget, block *lmsp.ProjectBlockObject) {
+	fmt.Fprint(w, getField(block, "field_flippermoremotor_multiple-port-selector"))
+}
+
+func visitInput(w io.Writer, target lmsp.ProjectTarget, block *lmsp.ProjectBlockObject, inputName string) {
+	inputs := block.Inputs.(map[string]interface{})
+	input := inputs[inputName].([]interface{})
+	switch val := input[1].(type) {
+	case string:
+		visitBlock(w, target, lmsp.ProjectBlockID(val))
+	case []interface{}:
+		id := int(val[0].(float64))
+		v := val[1].(string)
+		switch id {
+		case 4, 5, 6, 7, 8, 9:
+			fmt.Fprint(w, v)
+		case 10:
+			fmt.Fprintf(w, "%q", v)
+		case 11:
+			fmt.Fprintf(w, "[broadcast %q]", v)
+		case 12:
+			fmt.Fprintf(w, "[variable %q]", v)
+		case 13:
+			fmt.Fprintf(w, "[list %q]", v)
+		default:
+			fmt.Fprintf(w, "???%#v???", val)
+		}
 	}
 }
 
