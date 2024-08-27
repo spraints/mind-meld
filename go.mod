49f487c1df6bd01a3bdc07d071813a8624064b82
Matt Burke <spraints@gmail.com>
2024-08-26 21:37:45 -0400

    Use cobra

--- go.mod
+++ go.mod
@@ -6,5 +6,6 @@ require (
 	github.com/charmbracelet/bubbletea v0.16.0
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/pkg/errors v0.9.1
+	github.com/spf13/cobra v1.8.1
 	github.com/stretchr/testify v1.7.0
 )
--- main.go
+++ main.go
@@ -6,6 +6,8 @@ import (
 	"log"
 	"os"
 
+	"github.com/spf13/cobra"
+
 	"github.com/spraints/mind-meld/githooks"
 	"github.com/spraints/mind-meld/lmsdump"
 	"github.com/spraints/mind-meld/lmsp"
@@ -13,28 +15,58 @@ import (
 )
 
 func main() {
-	if len(os.Args) < 2 {
-		fmt.Printf("Usage: %s [dump FILE | browse]\n", os.Args[0])
-		os.Exit(1)
-	}
+	finish(mkRootCmd().Execute())
+}
 
-	switch os.Args[1] {
-	case "pre-commit":
-		mode := githooks.UpdateWorkingCopy
-		for _, arg := range os.Args[2:] {
-			if arg == "--cached" {
+func mkRootCmd() *cobra.Command {
+	root := &cobra.Command{
+		Use:   "mind-meld",
+		Short: "Manage your LEGO MINDSTORMS",
+	}
+	root.AddCommand(mkBrowseCmd())
+	root.AddCommand(mkDumpCmd())
+	root.AddCommand(mkPreCommitCmd())
+	return root
+}
+
+func mkBrowseCmd() *cobra.Command {
+	return &cobra.Command{
+		Use:   "browse",
+		Short: "Browse for a file and see inside of it!",
+		Args:  cobra.NoArgs,
+		RunE: func(*cobra.Command, []string) error {
+			return ui.Run()
+		},
+	}
+}
+
+func mkDumpCmd() *cobra.Command {
+	return &cobra.Command{
+		Use:   "dump",
+		Short: "Print a plain text version of a mindstorms program.",
+		Args:  cobra.ExactArgs(1),
+		RunE: func(_ *cobra.Command, args []string) error {
+			return dump(args[0])
+		},
+	}
+}
+
+func mkPreCommitCmd() *cobra.Command {
+	var cached bool
+	cmd := &cobra.Command{
+		Use:    "pre-commit",
+		Args:   cobra.NoArgs,
+		Hidden: true,
+		RunE: func(*cobra.Command, []string) error {
+			mode := githooks.UpdateWorkingCopy
+			if cached {
 				mode = githooks.UpdateCache
 			}
-		}
-		finish(githooks.RunPreCommit(mode))
-	case "dump":
-		finish(dump(os.Args[2]))
-	case "browse":
-		finish(ui.Run())
-	default:
-		fmt.Printf("Usage: %s [dump FILE]\n", os.Args[0])
-		os.Exit(1)
+			return githooks.RunPreCommit(mode)
+		},
 	}
+	cmd.PersistentFlags().BoolVar(&cached, "cached", false, "update index instead of working copy")
+	return cmd
 }
 
 func finish(err error) {
